58091d8e0eebd8a18bbb78f7f1ea64a557526419
Jonas Genao <jonas_genao@uri.edu>
2024-08-19 19:17:51 -0400

    Create patternBuilder.cpp

---
+++ patternBuilder.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+void makeStar(){
+
+}
+
+void makeRectangle(){
+
+}
+
+void makeTriangle(){
+
+
+}
+
+void makeRhombus(){
+
+}
+
+void makeRightTriangle(){
+
+}
+
+void makeLeftTriangle(){
+
+}
+
+void makeHeart(){
+
+}
+
+int main(){
+
+
+    
+
+    return 0;
+}
