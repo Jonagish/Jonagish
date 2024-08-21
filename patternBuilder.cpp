287f96f5c16d890ed981142dcd7e262e3ba86d18
Jonas Genao <jonas_genao@uri.edu>
2024-08-21 12:24:26 -0400

    Finished multiple functions

--- patternBuilder.cpp
+++ patternBuilder.cpp
@@ -8,18 +8,28 @@ using std::string;
 //initializing functions
 void makeStar();
 void makeRectangle(int rows, int cols);
-void makeTriangle();
-void makeRhombus();
-void makeRightTriangle();
-void makeLeftTriangle();
+void makeTriangle(int size);
+void makeRhombus(int size);
+void makeRightTriangle(int size);
+void makeLeftTriangle(int size);
 void makeHeart();
-
+void makeCXMtree(int size);
+void triangleinhalf(int size){
+    for(int i = 1; i <= size/2; i++){
+        for(int j = size - i; j > 0; j--){
+            cout << " ";
+        }
+        for(int k = 0; k < i; k++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+}
 
 
 int main(){
 
-    int rows;
-    int cols;
+    int rows, cols, size;
     bool run = true;
     string pattern;
 
@@ -44,14 +54,25 @@ int main(){
         }
         //Triangle pattern
         else if(pattern == "T"){
+            cout << "Enter Size: ";                                                                         
+            cin >> size;
+            makeTriangle(size);
             run = false;
         }
         //Right Triangle pattern
         else if(pattern == "RT"){
+            cout << "Enter Size: ";                                                                         
+            cin >> size;
+            makeRightTriangle(size);
+
             run = false;
         }
         //Left Trianlge pattern
         else if(pattern == "LT"){
+            cout << "Enter Size: ";                                                                         
+            cin >> size;
+            makeLeftTriangle(size);
+
             run = false;
         }
         //Heart pattern
@@ -60,6 +81,10 @@ int main(){
         }
         //Rhombus pattern
         else if(pattern == "RB"){
+            cout << "Enter Size: ";                                                                         
+            cin >> size;
+            makeRhombus(size);
+
             run = false;
         }
         //Rectangle Pattern
@@ -68,8 +93,16 @@ int main(){
             cin >> rows;
             cout << "Enter Columns: ";
             cin >> cols;
-
+            cout << '\n';
             makeRectangle(rows, cols);
+
+            run = false;
+
+        }
+        else if(pattern == "CXM"){
+            cout << "Enter Size: ";                                                                         
+            cin >> size;
+            makeCXMtree(size);
 
             run = false;
         }
@@ -83,10 +116,25 @@ int main(){
     return 0;
 }
 
+void makeCXMtree(int size){
+    triangleinhalf(size);
+    triangleinhalf(size);
+    triangleinhalf(size);
+
+    for(int i = 1; i <= size/2; i++){
+        for(int j = 1; j < size; j++){
+            cout << " ";
+        }
+        cout << "*" << '\n';
+
+    }
+}
+
+
 void makeStar(){
 
 }
-
+//Fucntion to print Rectangle
 void makeRectangle(int rows, int cols){
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
@@ -97,21 +145,55 @@ void makeRectangle(int rows, int cols){
     return; 
 }
 
-void makeTriangleint(){
-
-
+void makeTriangle(int size){
+    for(int i = 1; i <= size; i++){
+        for(int j = size - i; j > 0; j--){
+            cout << " ";
+        }
+        for(int k = 0; k < i; k++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
 }
 
-void makeRhombus(){
-
+void makeRhombus(int size){
+    triangleinhalf(size*2);
+    
+    for(int i = 1; i < size/2; i++){
+        for(int j = 1; j < i; j++){
+            cout << " ";
+        }
+        for(int k = size; k > 1; k--){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+}
+//Funciton to print Rigth Triangle
+void makeRightTriangle(int size){
+    for(int i = 1; i <= size; i++){
+        for(int j = 0; j < i; j++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
 }
 
-void makeRightTriangle(){
+//Function to make Left Triangle
+void makeLeftTriangle(int size){
+     for (int i = 0; i < size; i++) {
+        //white spaces
+        for (int j = 0; j < size - i - 1; j++) {
+            std::cout << " ";
+        }
 
-}
-
-void makeLeftTriangle(){
-
+        //stars
+        for (int j = 0; j < i + 1; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
 }
 
 void makeHeart(){
